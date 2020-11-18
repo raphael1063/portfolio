40cac4145ca66801c36a06a3c25831b505637b39
wjlim2 <sysoftwjlim2@gmail.com>
2020-11-18 18:43:25 +0900

    setting home adapter

--- app/src/main/java/com/robin1/portfolio/ui/home/HomeAdapter.kt
+++ app/src/main/java/com/robin1/portfolio/ui/home/HomeAdapter.kt
@@ -1,11 +1,30 @@
 package com.robin1.portfolio.ui.home
 
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.robin1.portfolio.databinding.ItemHomeBinding
+import com.robin1.portfolio.databinding.ItemHomeSubtitleBinding
+import com.robin1.portfolio.databinding.ItemHomeTitleBinding
 
-class HomeAdapter {
+class HomeAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
 
-    inner class HomeViewHolder(private val binding: ItemHomeBinding) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+
+    inner class HomeTitleViewHolder(private val binding: ItemHomeTitleBinding) {
+
+    }
+
+    inner class HomeSubtitleViewHolder(private val binding: ItemHomeSubtitleBinding) {
 
     }
 }
