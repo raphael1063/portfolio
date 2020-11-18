0e9ca63ce56e3288cec4e3062b4df7d66621e5ba
wjlim2 <sysoftwjlim2@gmail.com>
2020-11-18 18:36:08 +0900

    homeActivity working

---
+++ app/src/main/java/com/robin1/portfolio/data/entities/HomeItemModel.kt
@@ -0,0 +1,8 @@
+package com.robin1.portfolio.data.entities
+
+data class HomeItemModel(
+    /**
+     * Subject Title
+     */
+    var subjectTitle : String
+)
---
+++ app/src/main/java/com/robin1/portfolio/ui/home/HomeAdapter.kt
@@ -0,0 +1,11 @@
+package com.robin1.portfolio.ui.home
+
+import androidx.recyclerview.widget.RecyclerView
+import com.robin1.portfolio.databinding.ItemHomeBinding
+
+class HomeAdapter {
+
+    inner class HomeViewHolder(private val binding: ItemHomeBinding) {
+
+    }
+}
